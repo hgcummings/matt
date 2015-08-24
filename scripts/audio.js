8a8f38cd1ab766f9e208aa8c39a442e726bf0054
hgcummings <harry@xlr3.com>
2015-08-24 02:03:46 +0100

    Correct audio paths for deployed version

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,8 +1,8 @@
 define(function() {
     var enabled = true;
     
-    var stepSound = new Audio('../../audio/step.wav');
-    var wallSound = new Audio('../../audio/wall.wav');
+    var stepSound = new Audio('audio/step.wav');
+    var wallSound = new Audio('audio/wall.wav');
     
     var wrap = function(sound) {
         return function() {
